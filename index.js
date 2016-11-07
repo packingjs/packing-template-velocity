f496e5d65e9d46fd6764e6c5b3a923ac7d9a3c93
zhongzhi107 <zhong.zhi@163.com>
2016-11-07 23:00:10 +0800

    v1.0.3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var path = require('path');
 var url = require('url');
 var util = require('util');
-var assign = require('object-assign');
+var assign = require('object-assign-deep');
 var clearRequire = require('clear-require');
 var Velocity = require('velocity/lib/engine');
 
@@ -12,6 +12,7 @@ module.exports = function(options) {
     extension: '.vm',
     templates: '.',
     mockData: '.',
+    globalData: '__global.js',
     root: '.',
     macro: '.',
     rewriteRules: {}
@@ -21,20 +22,26 @@ module.exports = function(options) {
     var pathname = options.rewriteRules[urlObject.pathname] || urlObject.pathname;
     var templateAbsPath = path.resolve(path.join(options.templates, pathname));
     var dataAbsPath = path.resolve(path.join(options.mockData, pathname.replace(options.extension, '.js')));
+    var globalDataPath = path.resolve(path.join(options.mockData, options.globalData));
     if (fs.existsSync(templateAbsPath)) {
       var tpl = fs.readFileSync(templateAbsPath, {encoding: options.encoding});
-      var context = {};
-      if (fs.existsSync(dataAbsPath)) {
-        try {
-          var contextExport = require(dataAbsPath);
-          if (util.isFunction(contextExport)) {
-            context = contextExport(req, res);
-          } else {
-            context = contextExport;
-          }
+      if (fs.existsSync(templateAbsPath)) {
+      var globalContext = {};
+      if (fs.existsSync(globalDataPath)) {
+        var gcontext = require(globalDataPath);
+        if (util.isFunction(gcontext)) {
+          globalContext = gcontext(req, res);
+        } else {
+          globalContext = gcontext;
         }
-        catch (e) {
-          console.log('File "' + dataAbsPath + ' require failed.\n' + e);
+      }
+      var pageContext = {};
+      if (fs.existsSync(dataAbsPath)) {
+        var pcontext = require(dataAbsPath);
+        if (util.isFunction(pcontext)) {
+          pageContext = pcontext(req, res);
+        } else {
+          pageContext = pcontext;
         }
       }
       var compiledTpl = new Velocity({
@@ -42,8 +49,13 @@ module.exports = function(options) {
         template: templateAbsPath,
         macro: path.resolve(options.macro)
       });
-      var output = compiledTpl.render(context);
-      res.end(output);
+      try {
+        var output = compiledTpl.render(assign(globalContext, pageContext));
+        res.end(output);
+      } catch (e) {
+        console.log(e);
+        next();
+      }
     } else {
       next();
     }
